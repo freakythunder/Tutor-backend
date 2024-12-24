6b1edbe4435fd8047565e89256ff9cae2ba60564
Bikash Mohanty <vikash070310@gmail.com>
2024-12-24 19:49:38 +0530

    feature 2 roll out, adding chat section

---
+++ src/models/codeModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const codeSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
+  code: { type: String, required: true },
+  executionResult: { type: String, default: null },
+  error: { type: String, default: null },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Code = mongoose.model('Code', codeSchema);
+
+module.exports = Code;
